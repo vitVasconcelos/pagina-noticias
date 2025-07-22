4dddf366b5fb0fea5e9f58d7545d3d6032784a2b
Vitória Vasconcelos <vitoria.mvsv@gmail.com>
2025-07-21 21:11:16 -0300

    adicionei a responsividade e o javascript

---
+++ src/javascript/scripts.js
@@ -0,0 +1,45 @@
+function trocarImagem() {
+    const foto = document.getElementById('foto');
+    if (window.innerWidth <= 450) {
+      foto.src = "/src/imagens/image-web-3-mobile.jpg";
+    } else {
+      foto.src = "/src/imagens/image-web-3-desktop.jpg";
+    }
+  }
+  
+  function exibirMenuIcon() {
+    const icon = document.getElementById('menu-icon');
+
+    if (window.innerWidth <= 450) {
+      icon.style.display = 'block';
+  
+    } else {
+      icon.style.display = 'none';
+    }
+  }
+  
+  function controlarMenuLateral() {
+    const menuIcon = document.getElementById('menu-icon');
+    const menuLateral = document.getElementById('menu-lateral');
+    const fecharIcon = document.getElementById('fechar-menu');
+  
+    menuIcon.addEventListener('click', () => {
+      menuLateral.classList.add('ativo');
+    });
+  
+    fecharIcon.addEventListener('click', () => {
+      menuLateral.classList.remove('ativo');
+    });
+  }
+  
+  window.addEventListener("load", () => {
+    trocarImagem();
+    exibirMenuIcon();
+    controlarMenuLateral();
+  });
+  
+  window.addEventListener("resize", () => {
+    trocarImagem();
+    exibirMenuIcon();
+  });
+  
